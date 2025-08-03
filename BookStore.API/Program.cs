a86699035bcd463b7254b26f9d8a02ed0091fb35
Stanislav <dobryi_stas1998@mail.ru>
2025-08-03 20:18:03 +0300

    Добавил builder.Services.AddControllers(); (swagger заработал)

--- BookStore.API/Program.cs
+++ BookStore.API/Program.cs
@@ -6,6 +6,8 @@ using BookStore.DataAccess.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
